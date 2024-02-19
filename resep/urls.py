5c6cbdd3864dd7d3c3f1cc5db6e52a2b202723eb
Akbar <akbar@armsolusi.com>
2024-02-19 19:37:32 +0700

    Initial struktur file static

--- resep/urls.py
+++ resep/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
-from  .views import index
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', index)
+    path('tambah_roti/', views.tambah_roti, name='tambah_roti'),
+    path('daftar_roti/', views.daftar_roti, name='daftar_roti'),
+    path('tambah_penjualan/', views.tambah_penjualan, name='tambah_penjualan'),
+    path('daftar_penjualan/', views.daftar_penjualan, name='daftar_penjualan'),
+    path('tambah_bahan_baku/', views.tambah_bahan_baku, name='tambah_bahan_baku'),
+    path('daftar_bahan_baku/', views.daftar_bahan_baku, name='daftar_bahan_baku'),
 ]
