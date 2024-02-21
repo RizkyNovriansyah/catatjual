78866626a6b7c259db48b5b49f3473f301be372a
rinov <rinov>
2024-02-22 05:48:45 +0700

    prepare tampilan

--- base/urls.py
+++ base/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catat/', include('catat.urls')),
+    
+    path('', include('catat.urls')),
     path('resep/', include('resep.urls')),
 ]
--- resep/views.py
+++ resep/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    pesanans = []
+    return render(request, 'index.html', locals())
