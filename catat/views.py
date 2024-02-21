0b5cdafde5b9493718c13c4d6587dd509a06bd0a
rinov <rinov>
2024-02-22 06:08:27 +0700

    init template

--- catat/views.py
+++ catat/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pesanans = []
-    return render(request, 'index.html', locals())
+    
+    return render(request, 'catat/dashboard.html')
