96b7ad1f6d4d096f242f859d2b3a27668ad172be
Akbar <akbar@armsolusi.com>
2024-03-01 14:45:31 +0700

    Try css form django

--- resep/forms.py
+++ resep/forms.py
@@ -15,10 +15,22 @@ class MasterBahanForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['kode_bahan'].label = 'Kode Bahan'
         self.fields['nama'].label = 'Nama Bahan'
+        self.fields['total'].label = 'total'
+        self.fields['qty_keseluruhan'].label = 'qty_keseluruhan'
+        self.fields['qty_terkecil'].label = 'qty_terkecil'
+        self.fields['harga'].label = 'harga'
+        self.fields['harga_jual'].label = 'harga_jual'
 
         # add css in nama
+        self.fields['kode_bahan'].widget.attrs.update({'class':'form-control'})
         self.fields['nama'].widget.attrs.update({'class':'form-control'})
+        self.fields['total'].widget.attrs.update({'class':'form-control'})
+        self.fields['qty_keseluruhan'].widget.attrs.update({'class':'form-control'})
+        self.fields['qty_terkecil'].widget.attrs.update({'class':'form-control'})
+        self.fields['harga'].widget.attrs.update({'class':'form-control'})
+        self.fields['harga_jual'].widget.attrs.update({'class':'form-control'})
 
     def save(self, commit=True):
         instance = super().save(commit=False)
