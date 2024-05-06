0281da8c98b2198b73fd7e310d37d5b5fe63fee9
Akbar <alhazir.akbar00@gmail.com>
2024-05-06 08:52:35 +0700

    Dokumentasi Code Resep

--- resep/views.py
+++ resep/views.py
@@ -12,105 +12,127 @@ from django.urls import reverse_lazy
 
 #Bahan
 class BahanCreate(CreateView):
+    # Menentukan model yang digunakan untuk membuat objek.
     model = MasterBahan
+    # Menentukan form class yang digunakan untuk membuat form.
     form_class = MasterBahanForm
+    # Menentukan URL yang akan diarahkan setelah pembuatan objek berhasil.
     success_url = reverse_lazy('bahan_list')
     
+    # Override method form_valid untuk memproses form yang valid.
     def form_valid(self, form):
+        # Mendapatkan nilai harga dari form.
         harga = form.cleaned_data['harga']
+        # Mendapatkan nilai quantity keseluruhan dari form.
         quantity = form.cleaned_data['qty_keseluruhan']
+        # Mendapatkan nilai quantity terkecil dari form.
         quantity_terkecil = form.cleaned_data['qty_terkecil']
         
+        # Menghitung harga per kilogram jika quantity tidak sama dengan 0.
         if quantity != 0:
             harga_kg = harga / quantity
+            # Menghitung harga per gram berdasarkan harga per kilogram.
             harga_gram = harga_kg / quantity_terkecil
         else:
+            # Jika quantity sama dengan 0, set harga per kilogram dan harga per gram menjadi 0.
             harga_kg = 0
             harga_gram = 0
         
-        
+        # Menyimpan nilai harga per kilogram dan harga per gram ke instance form.
         form.instance.harga_kg = harga_kg
         form.instance.harga_gram = harga_gram
         
+        # Memanggil method form_valid dari superclass untuk melanjutkan proses validasi form.
         return super(BahanCreate, self).form_valid(form)
     
+# Kelas untuk menampilkan daftar bahan.
 class BahanList(ListView):
+    # Menentukan model yang akan digunakan untuk menampilkan daftar.
     model = MasterBahan
+    # Menentukan nama template yang akan digunakan untuk render halaman.
     template_name = 'resep/masterbahan_list.html'
+    # Menentukan nama objek konteks yang akan digunakan di template.
     context_object_name = 'bahans'
 
-class BahanCreate(CreateView):
-    model = MasterBahan
-    form_class = MasterBahanForm
-    success_url = reverse_lazy('bahan_list')
-    
-    def form_valid(self, form):
-        harga = form.cleaned_data['harga']
-        quantity = form.cleaned_data['qty_keseluruhan']
-        quantity_terkecil = form.cleaned_data['qty_terkecil']
-        
-        if quantity != 0:
-            harga_kg = harga / quantity
-        else:
-            harga_kg = 0
-        
-        if quantity_terkecil != 0:
-            harga_gram = harga_kg / quantity_terkecil
-        else:
-            harga_gram = 0
-        
-        form.instance.harga_kg = harga_kg
-        form.instance.harga_gram = harga_gram
-        
-        return super(BahanCreate, self).form_valid(form)
-
-    
+# Kelas untuk mengupdate data bahan.
 class BahanUpdate(UpdateView):
+    # Menentukan model yang akan diupdate.
     model = MasterBahan
+    # Menentukan fields yang bisa diubah pada proses update.
     fields = ['kode_bahan', 'nama', 'total', 'qty_keseluruhan', 'qty_terkecil', 'harga', 'harga_jual']
+    # Menentukan URL yang akan diarahkan setelah proses update berhasil.
     success_url = reverse_lazy('bahan_list')
     
+# Kelas untuk menghapus data bahan.
 class BahanDelete(DeleteView):
+    # Menentukan model yang akan dihapus.
     model = MasterBahan
+    # Menentukan nama objek konteks yang akan digunakan di template.
     context_object_name = 'bahan'
+    # Menentukan URL yang akan diarahkan setelah proses penghapusan berhasil.
     success_url = reverse_lazy('bahan_list')
 
+# Kelas untuk menampilkan detail bahan.
 class BahanDetail(DetailView):
+    # Menentukan model yang akan ditampilkan detailnya.
     model = MasterBahan
+    # Menentukan nama template yang akan digunakan untuk render halaman.
     template_name = 'resep/masterbahan_detail.html'
+    # Menentukan nama objek konteks yang akan digunakan di template.
     context_object_name = 'bahan'
     
+# Kelas untuk menampilkan daftar resep.
 class ResepList(ListView):
+    # Menentukan model yang akan digunakan untuk menampilkan daftar.
     model = BarangJadi
+    # Menentukan nama template yang akan digunakan untuk render halaman.
     template_name = 'resep/resep_list.html'
+    # Menentukan nama objek konteks yang akan digunakan di template.
     context_object_name = 'barang_jadis'
     
+# Kelas untuk mengupdate data resep.
 class ResepUpdate(UpdateView):
+    # Menentukan model yang akan diupdate.
     model = BarangJadi
+    # Menentukan fields yang bisa diubah pada proses update.
     fields = ['nama', 'harga_jual', 'hpp']
+    # Menentukan URL yang akan diarahkan setelah proses update berhasil.
     success_url = reverse_lazy('resep_list')
 
+# Kelas untuk menghapus data resep.
 class ResepDelete(DeleteView):
+    # Menentukan model yang akan dihapus.
     model = BarangJadi
+    # Menentukan nama objek konteks yang akan digunakan di template.
     context_object_name = 'barang_jadi'
+    # Menentukan URL yang akan diarahkan setelah proses penghapusan berhasil.
     success_url = reverse_lazy('resep_list')
 
+# Kelas untuk menampilkan detail resep.
 class ResepDetail(DetailView):
+    # Menentukan model yang akan ditampilkan detailnya.
     model = BarangJadi
+    # Menentukan nama template yang akan digunakan untuk render halaman.
     template_name = 'resep/resep_detail.html'
+    # Menentukan nama objek konteks yang akan digunakan di template.
     context_object_name = 'barang_jadi'   
     
-    # get daftar bahan
+    # Method untuk mendapatkan data bahan yang digunakan dalam resep.
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Menghitung selisih antara harga jual dan harga pokok penjualan.
         selisih = self.object.harga_jual - self.object.hpp
+        # Mendapatkan id resep.
         id_bahan = self.object.id
+        # Mendapatkan daftar bahan yang digunakan dalam resep.
         resep = Resep.objects.filter(barang_jadi__id=id_bahan)
         
         bahans = []
+        # Loop untuk setiap bahan dalam resep.
         for bahan in resep:
             nama = bahan.master_bahan.nama
             jumlah = bahan.jumlah_pemakaian
+            # Menghitung total harga pokok penjualan untuk satu bahan.
             total_hpp_single_bahan = bahan.master_bahan.harga_gram * jumlah
             bahans.append({'nama' : nama, 
                            'jumlah' : jumlah,
@@ -119,31 +141,36 @@ class ResepDetail(DetailView):
                            'total_hpp_single_bahan' : total_hpp_single_bahan,
                            })
             
-        # return bahans and context
+        # Menyimpan daftar bahan dan selisih ke dalam konteks.
         context['bahans'] = bahans
         context['selisih'] = selisih
         return context
-        
 
-# JsonResponse
+# JsonResponse untuk merespons data dalam format JSON
 from django.http import JsonResponse
+
+# Fungsi untuk mengecek detail bahan dan merespons dalam format JSON
 def cek_bahan(request, id):
-    # Mengambil objek bahan dari database berdasarkan id, response dengan jsonj
+    # Mengambil objek bahan dari database berdasarkan id
     bahan = MasterBahan.objects.get(id=id)
+    # Menyiapkan data bahan dalam format JSON
     result = {
-    "kode_bahan" : bahan.kode_bahan,
-    "nama" : bahan.nama,
-    "total" : bahan.total,
-    "qty_keseluruhan" : bahan.qty_keseluruhan,
-    "qty_terkecil" : bahan.qty_terkecil,
-    "harga" : bahan.harga,
-    "harga_jual" : bahan.harga_jual,
-    "harga_kg" : bahan.harga_kg,
-    "harga_gram" : bahan.harga_gram,
-    "created_date" : bahan.created_date,
-    "updated_date" : bahan.updated_date}
+        "kode_bahan": bahan.kode_bahan,
+        "nama": bahan.nama,
+        "total": bahan.total,
+        "qty_keseluruhan": bahan.qty_keseluruhan,
+        "qty_terkecil": bahan.qty_terkecil,
+        "harga": bahan.harga,
+        "harga_jual": bahan.harga_jual,
+        "harga_kg": bahan.harga_kg,
+        "harga_gram": bahan.harga_gram,
+        "created_date": bahan.created_date,
+        "updated_date": bahan.updated_date
+    }
+    # Mengirimkan response dalam format JSON
     return JsonResponse(result)
 
+# Fungsi untuk membuat resep baru
 def resep_create(request):
     # Mengambil semua bahan yang belum dihapus dari database
     bahans = MasterBahan.objects.filter(is_deleted=False)
@@ -194,21 +221,21 @@ def resep_create(request):
                 }
                 daftar_bahan.append(bahan)
                 
+                # Simpan data resep ke dalam database
                 resep_create = Resep.objects.create(
                     master_bahan = bahan_obj,
                     barang_jadi  = barang_jadi, 
                     jumlah_pemakaian = jumlah_satuan_list[i],
                 )
+            
             # Mengubah daftar bahan menjadi format JSON
-            # daftar_bahan_json = json.loads(barang_jadi.daftar_bahan)
             daftar_bahan_json = json.dumps(daftar_bahan)
-            daftar_bahan_obj = json.loads(daftar_bahan_json)
+            # Menyimpan daftar bahan dalam bentuk JSON ke dalam objek barang_jadi
             barang_jadi.daftar_bahan = daftar_bahan_json
             barang_jadi.save()
             print('daftar_bahan: ', daftar_bahan)
             
-
-            # Redirect ke halaman daftar bahan
+            # Redirect ke halaman detail resep
             return redirect('resep_detail', pk=barang_jadi.id)
 
     # Mengirimkan data bahan dan form ke template
