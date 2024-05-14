247599d47749c9019c76c653eba1e01f67995364
Akbar <alhazir.akbar00@gmail.com>
2024-05-14 19:47:51 +0700

    initial tampilan dashboard

--- catat/urls.py
+++ catat/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from  .views import index
+from  .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', dashboard, name='dashboard')
 ]
--- catat/views.py
+++ catat/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+import json
+from django.db.models.query import QuerySet
+from django.views import View  
+from django.urls import reverse_lazy
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from resep.models import BarangJadi, MasterBahan, Resep
 
-# Create your views here.
-def index(request):
-    
-    return render(request, 'catat/dashboard.html')
+
+def dashboard(request):
+    barang_jadi_count = BarangJadi.objects.count()
+    resep_count = Resep.objects.count()
+    bahan_count = MasterBahan.objects.count()
+    context = {'bahan_count': bahan_count,
+               'resep_count': resep_count,
+               'barang_jadi_count': barang_jadi_count}
+    return render(request, 'catat/dashboard.html', context)
