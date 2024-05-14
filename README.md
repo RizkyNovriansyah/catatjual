5a50a6d6915b33d1197eb8b5cc825dc5c8c893e4
Akbar <alhazir.akbar00@gmail.com>
2024-05-14 22:53:24 +0700

    fix import

--- README.md
+++ README.md
@@ -18,3 +18,14 @@
 
 6. Delete all bahan:
    python manage.py delete_bahan
+
+7. Import Resep dilakukan setelah import bahan
+   python manage.py import_resep {Lokasi File excel}/import_resep.xlsx
+   Example :
+   python manage.py import_resep {/home/enigma/code/github/catatjual}/import_resep.xlsx
+
+8. Export Resep
+   python manage.py export_resep
+   file excel disimpan pada folder data
+   Example :
+   Export Resep done! File saved at: data/export_resep_2024-05-14.xlsx
--- resep/management/commands/import_resep.py
+++ resep/management/commands/import_resep.py
@@ -3,39 +3,55 @@ from django.core.management.base import BaseCommand
 from resep.models import BarangJadi, MasterBahan, Resep
 
 class Command(BaseCommand):
-    help = 'Import data from Excel file'
+    help = 'Impor data dari file Excel'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_path', type=str, help='Path to the Excel file')
+        # Menambahkan argumen file_path pada perintah manage.py
+        parser.add_argument('file_path', type=str, help='Path ke file Excel')
 
     def handle(self, *args, **kwargs):
         file_path = kwargs['file_path']
         
-        # Load the workbook
+        # Memuat workbook
         workbook = openpyxl.load_workbook(file_path)
         
-        # Assuming the data is in the first two sheets as per export mechanism
+        # Mengasumsikan data ada di dua lembar pertama sesuai mekanisme ekspor
         worksheet_barang_jadi = workbook["Barang Jadi"]
         worksheet_resep = workbook["Resep"]
-        
-        # Process data for Barang Jadi
-        for row in worksheet_barang_jadi.iter_rows(min_row=2, values_only=True):
-            barang_jadi = BarangJadi.objects.create(
-                nama=row[0],
-                kode_barang=row[1],
-                harga_jual=row[2],
-                hpp=row[3],
-                master_bahan=row[4]
-            )
-        
-        # Process data for Resep
-        for row in worksheet_resep.iter_rows(min_row=2, values_only=True):
-            barang_jadi = BarangJadi.objects.get(kode_barang=row[0])  # Assuming kode_barang is unique
-            master_bahan = MasterBahan.objects.get(kode_bahan=row[4])  # Assuming kode_bahan is unique
-            Resep.objects.create(
-                barang_jadi=barang_jadi,
-                master_bahan=master_bahan,
-                jumlah_pemakaian=row[2],
-            )
 
-        self.stdout.write(self.style.SUCCESS('Import data done!'))
+        # Memproses data untuk Barang Jadi
+        for row in worksheet_barang_jadi.iter_rows(min_row=2, values_only=True):
+            kode_barang_check = BarangJadi.objects.filter(kode_barang=row[1])
+            if kode_barang_check:    
+                print('Barang sudah ada, jadi di skip: ', kode_barang_check.first())
+                barang_already_exist = True
+            else:
+                barang_jadi = BarangJadi.objects.create(
+                    nama=row[0],
+                    kode_barang=row[1],
+                    harga_jual=row[2],
+                    hpp=row[3],
+                    master_roti=row[4]
+                )
+                barang_already_exist = False
+            
+        # Memproses data untuk Resep jika Barang Jadi belum ada
+        if not barang_already_exist:
+            for row in worksheet_resep.iter_rows(min_row=2, values_only=True):
+                try:
+                    barang_jadi = BarangJadi.objects.get(kode_barang=row[0])
+                    master_bahan = MasterBahan.objects.get(kode_bahan=row[4])
+                except BarangJadi.DoesNotExist:
+                    print(f"BarangJadi dengan kode_barang {row[0]} Gagal Di simpan.")
+                    continue
+                except MasterBahan.DoesNotExist:
+                    print(f"MasterBahan dengan kode_bahan {row[4]} tidak ditemukan.")
+                    continue
+
+                Resep.objects.create(
+                    barang_jadi=barang_jadi,
+                    master_bahan=master_bahan,
+                    jumlah_pemakaian=row[2],
+                )
+
+        self.stdout.write(self.style.SUCCESS('Impor data selesai!'))
