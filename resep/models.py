88dfffa941c5bf99c35db007943b63a26e9845c8
Akbar <alhazir.akbar00@gmail.com>
2024-06-22 20:32:11 +0700

    fix

--- resep/models.py
+++ resep/models.py
@@ -40,6 +40,19 @@ class MasterBahan(models.Model):
     def get_updated_date(self):
         return timezone.localtime(self.updated_date)
 
+class BahanOlahan(models.Model):
+    nama = models.CharField(max_length=100, blank=True, null=True)
+    qty_keseluruhan = models.IntegerField(blank=True, null=True, default=0) 
+    qty_terkecil = models.IntegerField(blank=True, null=True, default=0) 
+    harga_kg = models.IntegerField(blank=True, null=True, default=0)    
+    harga_gram = models.IntegerField(blank=True, null=True, default=0) 
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.nama
+
 class Resep(models.Model):
     master_bahan = models.ForeignKey(MasterBahan,blank=True, null=True, on_delete=models.CASCADE, related_name='MasterBahan')
     barang_jadi = models.ForeignKey(BarangJadi,blank=True, null=True, on_delete=models.CASCADE, related_name='BarangJadi')
@@ -48,3 +61,14 @@ class Resep(models.Model):
     
     def __str__(self):
         return f"{self.master_bahan.nama} - {self.barang_jadi.nama}"
+    
+class ResepBahanOlahan(models.Model):
+    resep = models.ForeignKey(Resep, on_delete=models.CASCADE, related_name='Resep')
+    bahan_olahan = models.ForeignKey(BahanOlahan, on_delete=models.CASCADE, related_name='BahanOlahan')
+    qty = models.IntegerField(blank=True, null=True, default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.resep} - {self.bahan_olahan}"
