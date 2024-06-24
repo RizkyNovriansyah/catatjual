91a6d9f9782ab7e6639c9de7e27ee76a65333687
Akbar <alhazir.akbar00@gmail.com>
2024-06-24 16:47:40 +0700

    fix form

--- resep/forms.py
+++ resep/forms.py
@@ -48,26 +48,18 @@ class MasterBahanForm(forms.ModelForm):
 class BahanOlahanForm(forms.ModelForm):
     class Meta:
         model = MasterBahan
-        fields = ['kode_bahan', 'nama', 'total', 'qty_keseluruhan', 'qty_terkecil', 'harga', 'harga_jual']
+        fields = ['nama', 'qty_keseluruhan', 'qty_terkecil']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['kode_bahan'].label = 'Kode Bahan'
         self.fields['nama'].label = 'Nama Bahan'
-        self.fields['total'].label = 'total'
         self.fields['qty_keseluruhan'].label = 'qty_keseluruhan'
         self.fields['qty_terkecil'].label = 'qty_terkecil'
-        self.fields['harga'].label = 'harga'
-        self.fields['harga_jual'].label = 'harga_jual'
 
         # add css in nama
-        self.fields['kode_bahan'].widget.attrs.update({'class':'form-control','placeholder':"contoh : GG000"})
         self.fields['nama'].widget.attrs.update({'class':'form-control','placeholder':"contoh : Gula"})
-        self.fields['total'].widget.attrs.update({'class':'form-control','placeholder':"contoh : total"})
         self.fields['qty_keseluruhan'].widget.attrs.update({'class':'form-control check-harga','placeholder':"contoh : 10000"})
         self.fields['qty_terkecil'].widget.attrs.update({'class':'form-control','placeholder':"contoh : 100"})
-        self.fields['harga'].widget.attrs.update({'class':'form-control check-harga bantuan-rupiah','placeholder':"contoh : 500000","data-nama-bantuan":"harga-bantuan-rupiah"})
-        self.fields['harga_jual'].widget.attrs.update({'class':'form-control check-harga','placeholder':"contoh : "})
 
     def save(self, commit=True):
         instance = super().save(commit=False)
